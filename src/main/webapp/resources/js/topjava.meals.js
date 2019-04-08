843ad98dfdc7d514b5c7568e9428b671708f661c
SoulDrake <maxsouldrake@gmail.com>
2019-04-08 18:23:55 +0300

    HW8

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,37 @@
+const ajaxUrl = "ajax/meals/";
+
+$(function () {
+    makeEditable({
+            ajaxUrl: ajaxUrl,
+            datatableApi: $("#datatable").DataTable({
+                "paging": false,
+                "info": true,
+                "columns": [
+                    {
+                        "data": "dateTime"
+                    },
+                    {
+                        "data": "description"
+                    },
+                    {
+                        "data": "calories"
+                    },
+                    {
+                        "defaultContent": "Edit",
+                        "orderable": false
+                    },
+                    {
+                        "defaultContent": "Delete",
+                        "orderable": false
+                    }
+                ],
+                "order": [
+                    [
+                        0,
+                        "desc"
+                    ]
+                ]
+            })
+        }
+    );
+});
